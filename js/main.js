873b185b2c026dea7528a4a03c1116bedb90230b
alexandrajaramz <alexandra.jara.mz@gmail.com>
2019-08-05 17:25:35 +0200

    parse the input value, create label and connect it to the input

--- js/main.js
+++ js/main.js
@@ -1,40 +1,39 @@
 'use strict';
 
-const inputNumber = document.querySelector('.input');
+const input = document.querySelector('.input');
 const buttonTry = document.querySelector('.btn');
 const messages = document.querySelector('.tracks__content');
 const counter =  document.querySelector('.count');
+const myRandomNumber = getRandomNumber(100);
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-const myRandomNumber = getRandomNumber(100);
 
-console.log(`Mi número aleatorio es ${myRandomNumber}`);
+function gameStart () {
+    comparingNumbers();
+    counterAdd();
+}
 
 function comparingNumbers () {
-    console.log(inputNumber.value);
-    if (inputNumber.value < myRandomNumber) {
-        const lowNumber = 'Demasiado bajo';
-        console.log (lowNumber);
-        messages.innerHTML = lowNumber;
-    } else if (inputNumber.value > myRandomNumber) {
+    const inputValue = parseInt(input.value);
+    if (inputValue === myRandomNumber) {
+        const youWin = '¡HAS GANADO, CAMPEONA!';
+        messages.innerHTML = youWin;
+    } else if (inputValue > myRandomNumber) {
         const highNumber = 'Demasiado alto';
-        console.log (highNumber);
         messages.innerHTML = highNumber;
     } else {
-        const youWin = '¡HAS GANADO, CAMPEONA!';
-        console.log (youWin);
-        messages.innerHTML = youWin;
+        const lowNumber = 'Demasiado bajo';
+        messages.innerHTML = lowNumber;
     }
 }
 
-buttonTry.addEventListener('click', comparingNumbers);
-
 let i = 0;
+
 function counterAdd () {
  i += 1;
  counter.innerHTML = i;
 }
 
-buttonTry.addEventListener('click', counterAdd);
+buttonTry.addEventListener('click', gameStart);
