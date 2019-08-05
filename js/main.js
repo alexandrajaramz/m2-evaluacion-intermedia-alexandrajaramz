ee5d7393bb7c4bf7ff65b9d47cbb6d1bd2cf5b3d
alexandrajaramz <alexandra.jara.mz@gmail.com>
2019-08-05 17:56:32 +0200

    manage different situations and listen enter

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 const input = document.querySelector('.input');
 const buttonTry = document.querySelector('.btn');
-const messages = document.querySelector('.tracks__content');
+const messages = document.querySelector('.explain');
 const counter =  document.querySelector('.count');
 const myRandomNumber = getRandomNumber(100);
 
@@ -10,16 +10,18 @@ function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-function gameStart () {
-    comparingNumbers();
-    counterAdd();
-}
-
 function comparingNumbers () {
+    const stringValue = input.value;
     const inputValue = parseInt(input.value);
     if (inputValue === myRandomNumber) {
         const youWin = '¡HAS GANADO, CAMPEONA!';
         messages.innerHTML = youWin;
+    } else if (stringValue === '') {
+        messages.innerHTML = 'Si no escribes, mal vamos.';
+    } else if (inputValue < 0) {
+        messages.innerHTML = 'Te has quedao cortico.'
+    } else if (inputValue > 100) {
+        messages.innerHTML = 'Te has pasao tres pueblos.'
     } else if (inputValue > myRandomNumber) {
         const highNumber = 'Demasiado alto';
         messages.innerHTML = highNumber;
@@ -36,4 +38,16 @@ function counterAdd () {
  counter.innerHTML = i;
 }
 
+function gameStart () {
+    comparingNumbers();
+    counterAdd();
+}
+
+function fakeClick (event) {
+    if (event.keyCode === 13) {
+        gameStart();
+    }
+}
+
 buttonTry.addEventListener('click', gameStart);
+input.addEventListener('keyup', fakeClick);
